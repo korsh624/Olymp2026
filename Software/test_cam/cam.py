ff8df9e55e3b5b15aac461cf966338a86cbed177
korsh624 <korsh624@gmail.com>
2026-05-25 10:06:10 +0300

    add cam

--- Software/test_cam/cam.py
+++ Software/test_cam/cam.py
@@ -1,5 +1,5 @@
 import cv2
-cap=cv2.VideoCapture(1, cv2.CAP_DSHOW)
+cap=cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
@@ -7,7 +7,6 @@ while True:
      ret,frame=cap.read()
      frame = frame[450:, 250:950]
      cv2.imshow('frame',frame)
-     # Выделяем необходимый фрагмент: image[y1:y2, x1:x2
 
      if cv2.waitKey(1) & 0xFF == ord('q'):
          break
