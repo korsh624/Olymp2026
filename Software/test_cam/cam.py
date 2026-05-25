775e265805e5d76f83c63e4f026ebddd8654c314
korsh624 <korsh624@gmail.com>
2026-05-25 10:35:30 +0300

    add cam vieo0

--- Software/test_cam/cam.py
+++ Software/test_cam/cam.py
@@ -1,5 +1,5 @@
 import cv2
-cap=cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap=cv2.VideoCapture('/dev/video0', cv2.CAP_DSHOW)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
