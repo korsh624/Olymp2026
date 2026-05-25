8b27cd86c5eda94e7d630348511437efa8a31067
korsh624 <korsh624@gmail.com>
2026-05-25 10:39:59 +0300

    0

--- Software/test_cam/cam.py
+++ Software/test_cam/cam.py
@@ -1,6 +1,5 @@
 import cv2
-cap=cv2.VideoCapture('/dev/video0', cv2.CAP_DSHOW)
-cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
+cap=cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 while True:
